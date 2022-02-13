b100b80a344a6fe18d0a0202cc12735967992223
Sudhanshu Srivastava <sds997srivastava@gmail.com>
2022-02-13 13:55:47 +0530

    added template and css

--- Student_Portal/views.py
+++ Student_Portal/views.py
@@ -7,7 +7,8 @@ def home(request):
     return HttpResponse('<h1> Welcome to Student ERP </h1>')
 
 def index(request):
-    return HttpResponse('Hello World')
+    portal_name = {'portal':"Student ERP"}
+    return render(request,'Student_Portal\index.html',context=portal_name)
 
 def emphasized_HTML(request):
     return HttpResponse('<em> Hello World </em>')
