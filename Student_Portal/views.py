0dffbdc5da2eb32809ef47748add38cc410180ac
Sudhanshu Srivastava <sds997srivastava@gmail.com>
2022-02-13 14:00:52 +0530

    change in views.py

--- Student_Portal/views.py
+++ Student_Portal/views.py
@@ -7,8 +7,8 @@ def home(request):
     return HttpResponse('<h1> Welcome to Student ERP </h1>')
 
 def index(request):
-    portal_name = {'portal':"Student ERP"}
-    return render(request,'Student_Portal\index.html',context=portal_name)
+    portal_nam = {'portal':"Student ERP"}
+    return render(request,'Student_Portal\index.html',context=portal_nam)
 
 def emphasized_HTML(request):
     return HttpResponse('<em> Hello World </em>')
